fbb255d315109b71bc619fc9199a18d1c6ec2b12
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-07-29 16:20:11 +0300

    JS: introduce a new base class for generated tests

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1097,12 +1097,20 @@ fun main(args: Array<String>) {
             model("reservedWords/cases")
         }
 
+        testClass<AbstractFunctionCallableReferenceTest> {
+            model("callableReference/function/cases")
+        }
+
+        testClass<AbstractPropertyCallableReferenceTest> {
+            model("callableReference/property/cases")
+        }
+
         testClass<AbstractDynamicTest>() {
             model("dynamic/cases")
         }
 
         testClass<AbstractMultiModuleTest>() {
-            model("multiModule/cases", extension = null, recursive =false)
+            model("multiModule/cases")
         }
 
         testClass<AbstractInlineJsTest>() {
@@ -1118,7 +1126,7 @@ fun main(args: Array<String>) {
         }
 
         testClass<AbstractInlineMultiModuleTest>() {
-            model("inlineMultiModule/cases", extension = null, recursive =false)
+            model("inlineMultiModule/cases", extension = null, recursive = false)
         }
 
         testClass<AbstractLabelTest>() {
